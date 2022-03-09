004e97af0c1b98e160ab4641d56402aba5e25a2b
TheBangOperator <classicjakeeod@gmail.com>
2022-03-09 14:18:27 -0800

    temp ex complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  const cTemp = (fTemp - 32) * (5/9);
+  let roundcTemp = cTemp.toFixed(1);
+  const displayTemp = Number(roundcTemp)
+  return displayTemp;
 };
 
-const ctof = function() {
+
+
+
+const ctof = function(cTemp) {
+  const fTemp = cTemp * (9/5) + 32;
+  let roundfTemp = fTemp.toFixed(1);
+  const displayTemp = Number(roundfTemp);
+  return displayTemp;
 
 };
 
