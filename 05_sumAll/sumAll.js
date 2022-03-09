57d876cc5710572d762ed48e79d2d620414d1067
TheBangOperator <classicjakeeod@gmail.com>
2022-03-09 12:28:17 -0800

    leap year ex. completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function(min,max) {
+    if(!Number.isInteger(min)|| !Number.isInteger(max)) return "ERROR";
+    if(min > max){
+        let temp = min;
+        min = max;
+        max = temp;
+    }
+    let sum = 0;
+    for (let i=min; i < max + 1; i++){
+        sum+=i;
+        if (sum < 0) return "ERROR";
+
+    };
+    return sum;
 
 };
+// npm test sumAll.spec.js
 
 // Do not edit below this line
 module.exports = sumAll;
