529013c05863493f840fb2b96014aafc06045bd8
TheBangOperator <classicjakeeod@gmail.com>
2022-03-09 11:04:36 -0800

    sumAll complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let array = args[0];
+    let newArray = [];
+    array.forEach((item) => {
+        if(!args.includes(item)){
+         newArray.push(item);
+ 
+        }
+    });
+  
+     return newArray;
+     
+   
+ };
+ 
+    // npm test removeFromArray.spec.js
 
-};
 
 // Do not edit below this line
 module.exports = removeFromArray;
