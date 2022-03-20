88062dbd6c5eb61e589e4cc21c399bb4739b984d
Jake <classicjakeeod@gmail.com>
2022-03-20 12:48:31 -0700

    Assignment complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,39 @@
-const add = function() {
+const add = function(a,b) {
+	return a+b;
+};
+
+const subtract = function(a,b) {
+	 return a-b;
+};
+
+const sum = function(array) {
+return array.reduce((total,current) => total +current,0);
+};
+
+const multiply = function(arr) {
+  return arr.length
+  ? arr.reduce((accumulator, nextItem) => accumulator*nextItem)
+  :0;
+
+};
+
+const power = function(a,b) {
+  return a**b;
 	
 };
 
-const subtract = function() {
-	
+const factorial = function(n) {
+	if (n===0 || n===1){
+    return 1;
+  } else {
+    for(let i = n - 1; i>=1; i--){
+      n*=i;
+    };
+    return n;
+  };
+
 };
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
-
+// npm test calculator.spec.js
 // Do not edit below this line
 module.exports = {
   add,
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    //lower case of string and removes non alphanumeric chars
+    let reg= /[\W]/g;
+    let lowCase = string.toLowerCase().replace(reg,'');
+    // turn lowercase letter string into array, reverse array, join array back together
+    let reverse = lowCase.split('').reverse().join('');
+    // compare
+    return reverse === lowCase ? true : false;
+
+
 
 };
 
+// npm test palindromes.spec.js
 // Do not edit below this line
 module.exports = palindromes;
